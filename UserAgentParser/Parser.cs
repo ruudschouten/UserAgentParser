adee57ffa85e8fd40980c083c3fd68702bcbc636
Ruud Schouten <ruud.schouten@outlook.com>
2017-02-06 20:12:57 +0100

    Created Class which keeps track of data inside User Agents. Created Class which parses the data.

---
+++ UserAgentParser/Parser.cs
@@ -0,0 +1,40 @@
+﻿namespace UserAgentParser {
+    public class Parser {
+        private static string GetMozillaCompatibility(string userAgent) {
+            return userAgent.Substring(0, userAgent.IndexOf("("));
+        }
+        private static string[] GetSystemDetails(string userAgent) {
+            int start = userAgent.IndexOf("(");
+            int end = userAgent.IndexOf(")");
+            return userAgent.Substring(start, end - start).Split(';');
+        }
+
+        private static string GetBrowserPlatform(string userAgent) {
+            int start = userAgent.IndexOf(")");
+            int end = userAgent.LastIndexOf("(");
+            return userAgent.Substring(start, end - start).Replace("(", "").Replace(")", "").Trim(' ');
+        }
+        private static string GetBrowserPlatformDetails(string userAgent) {
+            int start = userAgent.LastIndexOf("(");
+            int end = userAgent.LastIndexOf(")");
+            return userAgent.Substring(start, end - start).Replace("(", "").Replace(")", "").Trim(' ');
+        }
+
+        private static string GetEnhancement(string userAgent) {
+            int start = userAgent.LastIndexOf(")");
+            return userAgent.Substring(start).Replace(")", "").Replace("(", "").Trim(' ');
+        }
+
+        public static UserAgent Parse(string userAgent) {
+            string mozilla = GetMozillaCompatibility(userAgent);
+            string[] systemDetails = GetSystemDetails(userAgent);
+            string systemOs = systemDetails[0].TrimStart('(').Trim(' ');
+            string systemVersion = systemDetails[1].Trim(' ');
+            string systemDetailed = systemDetails[2].TrimEnd(')').Trim(' ');
+            string browserPlatform = GetBrowserPlatform(userAgent);
+            string browserPlatformDetails = GetBrowserPlatformDetails(userAgent);
+            string enhancement = GetEnhancement(userAgent);
+            return new UserAgent(userAgent, mozilla, systemOs, systemVersion, systemDetailed, browserPlatform, browserPlatformDetails, enhancement);
+        }
+    }
+}
---
+++ UserAgentParser/UserAgent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAgentParser {
+    public class UserAgent {
+        public string Source { get; set; }
+        public string MozillaVersion { get; set; }
+        public string System { get; private set; }
+        public string SystemVersion { get; private set; }
+        public string SystemDetailed { get; private set; }
+        public string Platform { get; set; }
+        public string BrowserPlatform { get; set; }
+        public string Enhancements { get; set; }
+
+        public UserAgent() { }
+
+        public UserAgent(string source, string mozilla, string system, string systemVersion, string systemDetailed, string platform, string browserPlatform, string enhancements) {
+            Source = source;
+            MozillaVersion = mozilla;
+            System = system;
+            SystemVersion = systemVersion;
+            SystemDetailed = systemDetailed;
+            Platform = platform;
+            BrowserPlatform = browserPlatform;
+            Enhancements = enhancements;
+        }
+
+        public string ToDetailedString() {
+            return $"Mozilla: {MozillaVersion}\nOS: {System} \nVersion: {SystemVersion} \nDetails: {SystemDetailed} \nPlatform: {Platform}\nBrowser Platform: {BrowserPlatform}\nEnhancements: {Enhancements}\n";
+        }
+
+        public override string ToString() {
+            return $"OS: {System} \nVersion: {SystemVersion} \nDetails: {SystemDetailed} \nPlatform: {Platform}\n";
+        }
+    }
+}
